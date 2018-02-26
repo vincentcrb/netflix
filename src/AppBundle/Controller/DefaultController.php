f445aa8ef60c290c727572e4b19dfc9b191ddd0b
vincentcrb <vincent.crb@gmail.com>
2018-02-26 17:02:23 +0100

    Ajout css

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Manager\MovieManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -20,13 +22,37 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/home/profil", name="profil")
+     * @Route("/home/profil", name="home")
      */
     public function indexAction2(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/home.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/home/movies", name="list_movies")
+     */
+    public function listMovie(MovieManager $moviesManager)
+    {
+        $movies = $moviesManager->getMovies();
+        return $this->render('admin/list-movies.html.twig', ['movies' => $movies]);
+    }
+
+    /**
+     * @Route("/home/movie/{id}", name="profil_movie")
+     */
+    public function profilMovie(MovieManager $moviesManager, $id)
+    {
+        $movie = $moviesManager->getMovie($id);
+
+        if($movie == null) {
+            throw new NotFoundHttpException('404, Film non trouvé');
+        }
+        return $this->render('admin/profil-movie.html.twig', [
+            'movie' => $movie
         ]);
     }
 }
