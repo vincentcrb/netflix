3d1401210efc984939f61566c03f215255008e0e
vincentcrb <vincent.crb@gmail.com>
2018-02-27 16:51:44 +0100

    Dashboard admin

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,12 +11,12 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin/dashboard", name="admin")
+     * @Route("/admin/dashboard", name="dashboard")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('admin/dashboard.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
